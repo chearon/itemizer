9e926ca8e79b92443dece82c6d331721a3fb159b
Caleb Hearon <caleb@chearon.net>
2021-06-27 09:36:09 -0400

    fix broken paths when used as a library

--- src/node.js
+++ src/node.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
+const path = require('path');
 const EmojiSegmenter = require('./emoji/emoji').EmojiSegmenter;
 const BidiSegmenter = require('./bidi/bidi').BidiSegmenter;
 const ScriptSegmenter = require('./script/script').ScriptSegmenter;
 const UnicodeTrie = require('unicode-trie');
 
-const trie = new UnicodeTrie(fs.readFileSync('./src/emoji/emoji_classes.trie'));
+const EMOJI_PATH = path.resolve(__dirname, 'emoji', 'emoji_classes.trie');
+const WASM_PATH = path.resolve(__dirname, '..', 'wasm', 'bundle.wasm');
 
-module.exports = WebAssembly.instantiate(fs.readFileSync('./wasm/bundle.wasm'), {})
+const trie = new UnicodeTrie(fs.readFileSync(EMOJI_PATH));
+
+module.exports = WebAssembly.instantiate(fs.readFileSync(WASM_PATH), {})
   .then(wasm => {
     wasm.instance.exports.malloc(4); // TODO re-check the buffer after every malloc
 
