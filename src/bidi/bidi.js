ef3463f08c9987a9f424d9fa90ab89e465d426f0
Caleb Hearon <caleb@chearon.net>
2022-11-05 12:21:41 -0400

    fix wrong bidi levels after \n

--- src/bidi/bidi.js
+++ src/bidi/bidi.js
@@ -1,6 +1,7 @@
 exports.BidiSegmenter = function ({instance}) {
   const {
     SBAlgorithmCreate,
+    SBAlgorithmGetParagraphBoundary,
     SBAlgorithmCreateParagraph,
     SBParagraphGetLevelsPtr,
     malloc,
@@ -11,6 +12,7 @@ exports.BidiSegmenter = function ({instance}) {
   function* iterate(str, initialLevel = 0) {
     const strPtr = malloc(str.length * 2);
     const seqPtr = malloc(12); // sizeof(SBCodepointSequence)
+    const twoInts = new Uint32Array(memory.buffer, malloc(8) /* sizeof(SBUInteger) * 2 */, 2);
 
     const strBuf = new Uint16Array(memory.buffer, strPtr, str.length);
     for (let i = 0; i < str.length; ++i) {
@@ -21,14 +23,29 @@ exports.BidiSegmenter = function ({instance}) {
     new Uint32Array(memory.buffer, seqPtr, 3).set([1, strPtr, str.length]);
 
     const algorithm = SBAlgorithmCreate(seqPtr);
-    const paragraph = SBAlgorithmCreateParagraph(algorithm, 0, -1, initialLevel);
-    const levels = new Uint8Array(memory.buffer, SBParagraphGetLevelsPtr(paragraph), str.length);
+    const paraLenPtr = twoInts.byteOffset;
+    const paraSepPtr = twoInts.byteOffset + 4;
+    let offset = 0;
+    let lastLevel;
+    while (offset < str.length) {
+      SBAlgorithmGetParagraphBoundary(algorithm, offset, str.length - offset, paraLenPtr, paraSepPtr);
+      const [paraLen, paraSep] = twoInts;
+      const paragraph = SBAlgorithmCreateParagraph(algorithm, offset, paraLen + paraSep, initialLevel);
+      const levels = new Uint8Array(memory.buffer, SBParagraphGetLevelsPtr(paragraph), paraLen + paraSep);
+      const isFirstParagraph = offset === 0;
+      const isLastParagraph = offset + paraLen + paraSep >= /* see Tehreer/SheenBidi#18 */ str.length;
+      let j = paraLen + paraSep;
 
-    let lastLevel = levels[0];
-    for (let i = 0; i <= str.length; ++i) {
-      const level = levels[i];
-      if (level !== lastLevel) yield {i, level: lastLevel};
-      lastLevel = level;
+      if (isFirstParagraph) lastLevel = levels[0];
+      if (isLastParagraph) j += 1; /* check levels[levels.length] to emit the final character */
+
+      for (let i = 0; i < j; ++i) {
+        const level = levels[i];
+        if (level !== lastLevel) yield {i: offset + i, level: lastLevel};
+        lastLevel = level;
+      }
+
+      offset += paraLen + paraSep
     }
 
     free(seqPtr);
